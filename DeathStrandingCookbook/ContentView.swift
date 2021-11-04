864cae24038bdf621b5e5d4385082f03988134d7
FanRende <zxcj04@gmail.com>
2021-11-05 03:24:08 +0800

    Initial Commit

---
+++ DeathStrandingCookbook/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DeathStrandingCookbook
+//
+//  Created by FanRende on 2021/11/5.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DeathStrandingCookbook/DeathStrandingCookbookApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeathStrandingCookbookApp.swift
+//  DeathStrandingCookbook
+//
+//  Created by FanRende on 2021/11/5.
+//
+
+import SwiftUI
+
+@main
+struct DeathStrandingCookbookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
